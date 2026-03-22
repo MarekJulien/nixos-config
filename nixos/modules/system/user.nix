3711072b53b26fe85dd2414d7dea6f8898c4bfcc
MarekJulien <166394893+MarekJulien@users.noreply.github.com>
2026-03-22 15:36:41 +0100

    Added basic module structure

---
+++ nixos/modules/system/user.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, lib, ... }:
+{
+  options.mainUser = {
+    username = lib.mkOption {
+      default = "beaver";
+    };
+  };
+
+  config = {
+    programs.zsh.enable = true;
+    users.users.${config.mainUser.username} = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      shell = pkgs.zsh;
+    };
+  };
+}
