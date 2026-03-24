7cd34ebac0592d1a1e066463ca9164bd804c4bda
MarekJulien <166394893+MarekJulien@users.noreply.github.com>
2026-03-24 19:09:11 +0100

    Dotfile links are linked now as well

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-find ./dotfiles -type f | while read file; do
+find ./dotfiles -type f,l | while read file; do
 	target="$HOME/${file#./dotfiles/}"
 	mkdir -pv "$(dirname "$target")"
 	ln -sfv "$(realpath $file)" "$target"
