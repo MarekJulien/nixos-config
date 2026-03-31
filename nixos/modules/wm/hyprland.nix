4809a2867cc848b0ebda743daacb3e875f7e4052
MarekJulien <166394893+MarekJulien@users.noreply.github.com>
2026-03-31 18:01:46 +0200

    Tried to make screenshare possible
    Just works with Discord in Brave so far
    Vesktop still broken
    No other applications tested

--- nixos/modules/wm/hyprland.nix
+++ nixos/modules/wm/hyprland.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 {
   programs.hyprland = {
-  enable = true;
-  xwayland.enable = true;
+    enable = true;
+    xwayland.enable = true;
   };
 
   environment.sessionVariables = {
@@ -10,12 +10,20 @@
     WLR_NO_HARDWARE_CURSORS = "1";
     # Hint electron apps to use wayland
     NIXOS_OZONE_WL = "1";
+    # Stuff from https://discourse.nixos.org/t/hyprland-screen-sharing/43658
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_TYPE = "wayland";
+    XDG_SESSION_DESKTOP = "Hyprland";
+    MOZ_ENABLE_WAYLAND = "1";
+    QT_QPA_PLATFORM = "wayland";
+    GDK_BACKEND = "wayland";
+    ELECTRON_ENABLE_WAYLAND = "1";
   };
 
   hardware = {
       graphics.enable = true;
       # Most wayland compositors need this
-      nvidia.modesetting.enable = true;
+      #nvidia.modesetting.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
@@ -43,11 +51,22 @@
   ];
 
   # TODO: Research
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    config = {
+      common.default = ["gtk"];
+      hyprland.default = ["gtk" "hyprland"];
+    };
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-wlr
+      pkgs.xdg-desktop-portal-hyprland
+    ];
+  };
+
 
   # TODO Modularize
-  # Sound
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -55,6 +74,7 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    wireplumber.enable = true;
   };
 
 }
