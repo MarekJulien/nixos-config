6d9f86cc1064050a7acd182b4d89b81680e205bc
MarekJulien <166394893+MarekJulien@users.noreply.github.com>
2026-04-20 23:04:38 +0200

    Added nvim telescope keybinds

--- nixos/nvf/config.nix
+++ nixos/nvf/config.nix
@@ -59,6 +59,77 @@ in
         key = "<Right>";
         action = "<Nop>";
       }
+      # Telescope 
+      # > File search
+      {
+        mode = [ "n" ];
+        key = "<leader>ff";
+        action = "<cmd>Telescope find_files<CR>";
+        desc = "Search files across the project";
+      }
+      {
+        mode = [ "n" ];
+        key = "<leader>fr";
+        action = "<cmd>Telescope oldfiles<CR>";
+        desc = "Search recently accessed files";
+      }
+      {
+        mode = [ "n" ];
+        key = "<leader>fb";
+        action = "<cmd>Telescope buffers<CR>";
+        desc = "Search open buffers";
+      }
+      # > Text search
+      {
+        mode = [ "n" ];
+        key = "<leader>fg";
+        action = "<cmd>Telescope live_grep<CR>";
+        desc = "Search file content across the project";
+      }
+      {
+        mode = [ "n" ];
+        key = "<leader>fs";
+        action = "<cmd>Telescope grep_string<CR>";
+        desc = "Search for the word under cursor across the project";
+      }
+      # > Help
+      {
+        mode = [ "n" ];
+        key = "<leader>fh";
+        action = "<cmd>Telescope help_tags<CR>";
+        desc = "Search Neovim help documentation (vim help tags)";
+      }
+      {
+        mode = [ "n" ];
+        key = "<leader>fk";
+        action = "<cmd>Telescope keymaps<CR>";
+        desc = "View keybindings";
+      }
+      {
+        mode = [ "n" ];
+        key = "<leader>fc";
+        action = "<cmd>Telescope commands<CR>";
+        desc = "Browse available Neovim commands";
+      }
+      # > Git
+      {
+        mode = [ "n" ];
+        key = "<leader>fgc";
+        action = "<cmd>Telescope git_commits<CR>";
+        desc = "Browse Git commit history";
+      }
+      {
+        mode = [ "n" ];
+        key = "<leader>fgb";
+        action = "<cmd>Telescope git_branches<CR>";
+        desc = "List and switch Git branches";
+      }
+      {
+        mode = [ "n" ];
+        key = "<leader>fgs";
+        action = "<cmd>Telescope git_status<CR>";
+        desc = "View git status";
+      }
     ];
     # Language support
     lsp.enable = true;
