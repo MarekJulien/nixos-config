eca4691ac6b19ad5daa925cdefe286bbdcac2f7e
MarekJulien <166394893+MarekJulien@users.noreply.github.com>
2026-04-20 22:15:59 +0200

    Fixed type in nixos-rebuild rofi menu

--- scripts/nixos-rebuild.sh
+++ scripts/nixos-rebuild.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-action=$(echo -e "test\nswitch" | rofi -dmenu -p "nixos.rebuild");
+action=$(echo -e "test\nswitch" | rofi -dmenu -p "nixos-rebuild");
 command="sudo nixos-rebuild ${action} --flake ~/nixos-config/nixos#${NIX_FLAKE_HOST}"
 
 alacritty -e bash -c "$command; printf '\n\nPress any key to close this window'; read -sn 1"
