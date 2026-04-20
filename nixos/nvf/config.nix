d702ec7679ebc3eb3c0c33541d2bce7f60c7d160
MarekJulien <166394893+MarekJulien@users.noreply.github.com>
2026-04-20 21:41:04 +0200

    Added some vim options and disabled arrow keys

--- nixos/nvf/config.nix
+++ nixos/nvf/config.nix
@@ -15,17 +15,51 @@ in
     # Theme
     theme = {
       enable = true;
-      name = "github"; # https://nvf.notashelf.dev/options.html#option-vim-theme-name
+      name = "github";                # https://nvf.notashelf.dev/options.html#option-vim-theme-name
       style = "dark_high_contrast";   # https://nvf.notashelf.dev/options.html#option-vim-theme-style
     };
-    # Vim settings
+    # Globals
+    globals = {
+      mapleader = " ";
+    };
+    # Options
     options = {
       number = true;
       relativenumber = true;
       tabstop = 2;
       shiftwidth = 2;
       expandtab = true;
+      ignorecase = true;
+      smartcase = true;
+      cursorline = true;
+      scrolloff = 10;
+      list = true;
+      confirm = true;
     };
+    # Keymaps
+    keymaps = [
+      # Disable arrow keys
+      {
+        mode = [ "n" "i" "v" ];
+        key = "<Up>";
+        action = "<Nop>";
+      }
+      {
+        mode = [ "n" "i" "v" ];
+        key = "<Down>";
+        action = "<Nop>";
+      }
+      {
+        mode = [ "n" "i" "v" ];
+        key = "<Left>";
+        action = "<Nop>";
+      }
+      {
+        mode = [ "n" "i" "v" ];
+        key = "<Right>";
+        action = "<Nop>";
+      }
+    ];
     # Language support
     lsp.enable = true;
     languages = {
