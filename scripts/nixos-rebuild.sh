ed5d0e61d0f78d17ce4e780867005ad32275f1e5
MarekJulien <166394893+MarekJulien@users.noreply.github.com>
2026-04-22 22:33:13 +0200

    Added rerun functionality to nixos-rebuild script

--- scripts/nixos-rebuild.sh
+++ scripts/nixos-rebuild.sh
@@ -3,4 +3,12 @@
 action=$(echo -e "test\nswitch" | rofi -dmenu -p "nixos-rebuild");
 command="sudo nixos-rebuild ${action} --flake ~/nixos-config/nixos#${NIX_FLAKE_HOST}"
 
-alacritty -e bash -c "$command; printf '\n\nPress any key to close this window'; read -sn 1"
+alacritty -e bash -c '
+while true; do
+  clear
+  eval "$0"
+  printf "\n\nPress r to rerun, any other key to close this window"
+  read -rsn 1 key
+  [[ $key == "r" ]] || break
+done
+' "$command"
