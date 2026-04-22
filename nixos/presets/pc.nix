cd74d245786604f11811723daa287c9b376deaff
MarekJulien <166394893+MarekJulien@users.noreply.github.com>
2026-04-22 22:33:58 +0200

    Fixed nix option evaluation mistake

--- nixos/presets/pc.nix
+++ nixos/presets/pc.nix
@@ -1,7 +1,7 @@
 { lib, config, ... }:
 
 {
-  config.custom = {
+  custom = {
     gui.enable = lib.mkDefault true;
     docker.enable = lib.mkDefault true;
     virtualisation.enable = lib.mkDefault true;
