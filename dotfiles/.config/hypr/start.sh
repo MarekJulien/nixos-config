7d8be6aa28377525c9053cde4b9e9b4981057aae
MarekJulien <git.marekjulien@pm.me>
2026-05-07 23:33:01 +0200

    Updated wallpaper workflow
    A default gets loaded on every startup now
    There is a dedicated hotkey to select a wallpaper now

--- dotfiles/.config/hypr/start.sh
+++ dotfiles/.config/hypr/start.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
+# waybar
+~/nixos-config/scripts/load-waybar.sh &
+
 # wallpaper daemon
+wallpapers="$HOME/Pictures/wallpapers"
 awww-daemon &
-awww img ~/Pictures/wallpapers/wallpaper.png
+awww img "${wallpapers}/default" &
+
+# notification daemon
+dunst &
 
 # networkmanager applet
 nm-applet --indicator &
-
-# waybar
-~/nixos-config/scripts/load-waybar.sh
-
-# notification daemon
-dunst
