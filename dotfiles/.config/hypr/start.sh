7b9612879d84b250e3ea88c0c917006ed1d2d53d
MarekJulien <git.marekjulien@pm.me>
2026-05-08 18:04:56 +0200

    Updated wallpaper script

--- dotfiles/.config/hypr/start.sh
+++ dotfiles/.config/hypr/start.sh
@@ -4,9 +4,7 @@
 ~/nixos-config/scripts/load-waybar.sh &
 
 # wallpaper daemon
-wallpapers="$HOME/Pictures/wallpapers"
 awww-daemon &
-awww img "${wallpapers}/default" &
 
 # notification daemon
 dunst &
