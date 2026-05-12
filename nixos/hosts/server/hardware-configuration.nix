ef3adb934f0df2b3c8391e1ea0784f24d8ed764b
MarekJulien <git.marekjulien@pm.me>
2026-05-12 20:27:16 +0200

    Added uas boot kernel module to server config
    Server can boot with new ssd now

--- nixos/hosts/server/hardware-configuration.nix
+++ nixos/hosts/server/hardware-configuration.nix
@@ -4,7 +4,7 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "uas" "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
