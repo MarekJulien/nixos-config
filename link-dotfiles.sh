95e3ca3c01cb904dc624e3b487ef2b6d775598e3
MarekJulien <git.marekjulien@pm.me>
2026-05-13 18:21:57 +0200

    Added ssh config to link-dotfiles script

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -2,8 +2,14 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# Link repo dotfiles
+echo "##### Link repo dotfiles #####"
 find "$SCRIPT_DIR/dotfiles" -type f,l | while read file; do
 	target="$HOME/${file#$SCRIPT_DIR/dotfiles/}"
 	mkdir -pv "$(dirname "$target")"
 	ln -sfv "$(realpath "$file")" "$target"
 done
+
+# Link ssh config
+echo "##### Link ssh config #####"
+ln -sfv "$HOME/keys/ssh/config" "$HOME/.ssh/config"
